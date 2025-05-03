3f3422179ad7f029802c323dd95273d0adda2f5f
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-05-03 17:43:51 +0200

    updated cors

--- src/server.js
+++ src/server.js
@@ -8,20 +8,22 @@ import * as API_ImageController from "./controllers/imageControllers.js";
 const app = express();
 
 const allowedOrigins = [
-    'http://127.0.0.1:5500/',
+    'http://127.0.0.1:5500',
     'http://localhost:5500',
-    'https://mauricehalsberghe.github.io/mytools/'
-  ];
+    'https://mauricehalsberghe.github.io'
+];
+  
 
   app.use(cors({
     origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error('Not allowed by CORS'));
+        console.log('Request origin:', origin); // Debug line
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.includes(origin)) {
+          return callback(null, true);
+        } else {
+          return callback(new Error('Not allowed by CORS'));
+        }
       }
-    }
   }));
 
 app.get('/', (req, res) => {
